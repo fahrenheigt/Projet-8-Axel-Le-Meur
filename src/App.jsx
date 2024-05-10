b7718583202abb3a094d69af113be272f4d40d50
Axel Le Meur <95547308+fahrenheigt@users.noreply.github.com>
2024-05-10 15:55:33 +0200

    Scss+ Routage ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Home from "./pages/Home/Home";
 import Error from "./pages/Error/Error";
 import About from "./pages/About/About";
 import Logement from "./pages/Logement/Logement";
-=======
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Header from "./components/header/Header.jsx";
-import Logement from "./pages/Logement.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Error from "./pages/Error.jsx";
->>>>>>> parent of 498aa27 (Organisation des fichiers et création du composant de navigation)
+import Header from "./components/Header/Header";
 
 function App() {
   return (
   <div className="App">
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+return(
+    <div className="flex flex-row space-between grow px-10">
+        <div>
+            <img src={logo} alt="" />
+        </div>
+        <div>
+            <div className="flex space-between gap-6 text-2xl">
+                <a href="/">Accueil</a>
+                <a href="/about">A propos</a>
+            </div>
+        </div>
+    </div>
+);
+}
